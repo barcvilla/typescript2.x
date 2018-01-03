dc919bc7dfeb6ace3e58a4f9ababb6692fd62677
Carlos Villanueva <ceva_19@hotmail.com>
2018-01-03 15:45:38 -0300

    Chapter 1 - Type Anotations

--- configuracion/app/app.ts
+++ configuracion/app/app.ts
@@ -1,1 +1,50 @@
 console.log("Hello World!!!");
+// 1.
+/**
+ * Usando palabra clave var
+ * var tiene alcance global donde se declare.
+ */
+function outerFunction()
+{
+    var value = 10;
+    function innerFuction()
+    {
+        console.log(value);
+    }
+    return innerFuction();
+}
+var func = outerFunction();
+func;
+
+//2.
+/**
+ * Usando palabra clave let & const
+ * A diferencia de var. let & const permite
+ * aplicar un alcance de la variable solo en el 
+ * bloque que ha sido declarado.
+ */
+function letExample(hasValue: boolean)
+{
+    if(hasValue)
+    {
+        let x = 14;
+        console.log(x);
+    }
+    console.log("");
+}
+letExample(true);
+
+//3.
+/**
+ * Type Annotation viene despues del identificador
+ * variable. Types puede ser tipo primitivo,
+ * array o tipos complejos usando clases e
+ * interfaces
+ */
+let numero: number = 42; // definimos var num
+//definimos el tipo de input params
+function example(name: string, age:number): number
+{
+    return 42;
+}
+console.log(example("hi", 35));
--- configuracion/js/app.js
+++ configuracion/js/app.js
@@ -1,3 +1,44 @@
 "use strict";
-
 console.log("Hello World!!!");
+// 1.
+/**
+ * Usando palabra clave var
+ * var tiene alcance global donde se declare.
+ */
+function outerFunction() {
+    var value = 10;
+    function innerFuction() {
+        console.log(value);
+    }
+    return innerFuction();
+}
+var func = outerFunction();
+func;
+//2.
+/**
+ * Usando palabra clave let & const
+ * A diferencia de var. let & const permite
+ * aplicar un alcance de la variable solo en el
+ * bloque que ha sido declarado.
+ */
+function letExample(hasValue) {
+    if (hasValue) {
+        var x = 14;
+        console.log(x);
+    }
+    console.log("");
+}
+letExample(true);
+//3.
+/**
+ * Type Annotation viene despues del identificador
+ * variable. Types puede ser tipo primitivo,
+ * array o tipos complejos usando clases e
+ * interfaces
+ */
+var numero = 42; // definimos var num
+//definimos el tipo de input params
+function example(name, age) {
+    return 42;
+}
+console.log(example("hi", 35));
