265a104484c3062fc09a59b3c35002bd03422f41
Carlos Villanueva <ceva_19@hotmail.com>
2018-06-08 19:30:46 -0300

    Creating Header and Footer Components

--- SportsNewsCombinator/src/app/core/header/header.component.ts
+++ SportsNewsCombinator/src/app/core/header/header.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-
+/**Definimos Component Decorator y enlazamos HTML y CSS */
 @Component({
   selector: 'snc-header',
   templateUrl: './header.component.html',
