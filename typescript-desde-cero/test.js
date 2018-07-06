7108735b19bcdb42395431918fa59c09e2586b5c
Carlos Villanueva <ceva_19@hotmail.com>
2018-07-06 19:29:53 -0300

    Curso TypeScript desde cero

---
+++ typescript-desde-cero/test.js
@@ -0,0 +1,10 @@
+console.log('Hola mundo TypeScript');
+/**
+ * Para la compilacion automatica de archivos typescript, escribir el comando
+ * tsc --watch "nombre del archivo typescrip"
+ */
+//declaramos una variable fuertemente tipadas en ts
+var s_nombre = 'Luis';
+var n_usersConectados = 50;
+var s_frase = "Mi frase\n                segunda linea\n                tercera linea\n                cuarta linea\n                quinta linea\n                sexta linea";
+console.log(s_frase);
